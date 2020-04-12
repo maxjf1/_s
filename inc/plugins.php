534df38bf05edb6a8f32c8415e876c40b1090601
maxjf1 <maxwellsouzacarvalho@gmail.com>
2020-04-12 19:07:11 -0300

    FIX plugins

--- inc/plugins.php
+++ inc/plugins.php
@@ -21,30 +21,30 @@ add_action( 'tgmpa_register', '_s_register_required_plugins' );
  * Register the required plugins for this theme.
  */
 function _s_register_required_plugins() {
-	$plugins = array(
+    $plugins = array(
         array(
-            'name'      => 'Campos personalizados',
-            'slug'      => 'advanced-custom-fields',
-            'required'  => true,
+            'name'     => 'Campos personalizados',
+            'slug'     => 'advanced-custom-fields',
+            'required' => true,
         ),
         array(
-            'name'      => 'Personalização do tema',
-            'slug'      => 'kirki',
-            'required'  => true,
+            'name'     => 'Personalização do tema',
+            'slug'     => 'kirki',
+            'required' => true,
         ),
-	);
-	/*
-	 * Array of configuration settings.
-	 */
-	$config = array(
-		'id'           => '_s',                 // Unique ID for hashing notices for multiple instances of TGMPA.
-		'menu'         => 'tgmpa-install-plugins', // Menu slug.
-		'has_notices'  => true,                    // Show admin notices or not.
-		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
-		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-		'message'      => '',                      // Message to output right before the plugins table.
-	);
+    );
+    /*
+     * Array of configuration settings.
+     */
+    $config = array(
+        'id'           => '_s',                    // Unique ID for hashing notices for multiple instances of TGMPA.
+        'menu'         => 'tgmpa-install-plugins', // Menu slug.
+        'has_notices'  => true,                    // Show admin notices or not.
+        'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+        'dismiss_msg'  => __( 'O tema pode não funcionar corretamente sem os plugins recomendados' ),                      // If 'dismissable' is false, this message will be output at top of nag.
+        'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+        'message'      => __( 'Instale os seguintes plugins recomendados' ),                      // Message to output right before the plugins table.
+    );
 
-	tgmpa( $plugins, $config );
+    tgmpa( $plugins, $config );
 }
